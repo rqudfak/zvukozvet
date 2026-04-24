4d0fc65434da15a085603057ede76f1ee320c20d
Anastasiya <nastya.davydova.2006@mail.ru>
2026-04-24 16:02:40 +0300

    .next

---
+++ frontend/.next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/21a0a4061f3ab64d.js",
+    "static/chunks/773abe17875a49cc.js",
+    "static/chunks/3b3c7ed019bbd741.js",
+    "static/chunks/turbopack-ec14c78cc7d204b8.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6EB+WJOMOjN2L5LMsqdSkdWa+u3b0GFTxtvvOacELmk=\"\n}"
